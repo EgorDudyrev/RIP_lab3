bdd12f96c7d075798a6b3d434592b07a5882f574
EgorDudyrev <egor.dudyrev@yandex.ru>
2017-09-23 19:33:57 +0300

    Complete ex.5

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -6,3 +6,11 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+from time import time
+
+class timer():
+    def __enter__(self):
+        self.start = time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(time()-self.start)
