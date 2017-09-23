71dfc7eaf2ff3600a3b43f16abb67cc6397a0eb3
EgorDudyrev <egor.dudyrev@yandex.ru>
2017-09-23 16:37:00 +0300

    Complete ex.2

--- ex_2.py
+++ ex_2.py
@@ -4,5 +4,21 @@ from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
+data3 = ['a','A','b','B']
 
 # Реализация задания 2
+print('data:',data1)
+print('unique:',', '.join(map(str,Unique(data1))))
+print()
+
+print('data:',data2)
+print('unique:',', '.join(map(str,Unique(data2))))
+print()
+
+print('data:',data3,'ignore_case:',True)
+print('unique:',', '.join(map(str,Unique(data3,ignore_case=True))))
+print()
+
+print('data:',data3,'ignore_case:',False)
+print('unique:',', '.join(map(str,Unique(data3,ignore_case=False))))
+print()
--- librip/iterators.py
+++ librip/iterators.py
@@ -7,11 +7,23 @@ class Unique(object):
         # Например: ignore_case = True, Aбв и АБВ разные строки
         #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
         # По-умолчанию ignore_case = False
-        pass
+        self.items = items
+        self.ignore_case = False
+        if 'ignore_case' in kwargs:
+            self.ignore_case = kwargs['ignore_case']
+        self.returned = []
 
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        # Нужно реализовать __next__
+        for item in self.items:
+            if item not in self.returned:
+                if self.ignore_case is True and type(item) == str:
+                    self.returned.append(item.lower())
+                    self.returned.append(item.upper())
+                else:
+                    self.returned.append(item)
+                return item
+        raise StopIteration()
 
     def __iter__(self):
         return self
